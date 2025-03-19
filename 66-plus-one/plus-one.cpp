e520fbf12861275e891aad2ce3dcd136aa09581e
Diksha Nautiyal <140512287+DikshaNautiyal06@users.noreply.github.com>
2025-03-19 13:49:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (88.06%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,32 +1,52 @@
 class Solution {
 public:
-    // Function to reverse a vector in-place
-    void reverse(vector<int>& arr) {
-        int s = 0, end = arr.size() - 1;
-        while (end > s) {
-            swap(arr[s++], arr[end--]);
-        }
-    }
-
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> ans;
-        int i = digits.size() - 1;
-        int carry = 1; // Start with 1, since we are adding one
-
-        while (i >= 0 || carry > 0) { 
-            int sum = carry; // Start with carry
-            if (i >= 0) {
-                sum += digits[i]; // Add digit if within bounds
-                i--;
-            }
-            carry = sum / 10; // Carry for next iteration
-            sum = sum % 10;    // Store last digit
-            ans.push_back(sum);
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--){
+        if(digits[i]==9){
+            digits[i]=0;
         }
-
-        // Reverse ans to get the correct order
-        reverse(ans);
-
-        return ans;
+        else{
+            digits[i]=digits[i]+1;
+            return digits;
+        }
+       
     }
+    digits.insert(digits.begin(),1);
+    return digits;
+    }
+
 };
+
+// class Solution {
+// public:
+    
+//     void reverse(vector<int>& arr) {
+//         int s = 0, end = arr.size() - 1;
+//         while (end > s) {
+//             swap(arr[s++], arr[end--]);
+//         }
+//     }
+
+//     vector<int> plusOne(vector<int>& digits) {
+//         vector<int> ans;
+//         int i = digits.size() - 1;
+//         int carry = 1; 
+
+//         while (i >= 0 || carry > 0) { 
+//             int sum = carry;
+//             if (i >= 0) {
+//                 sum += digits[i]; 
+//                 i--;
+//             }
+//             carry = sum / 10; 
+//             sum = sum % 10;    
+//             ans.push_back(sum);
+//         }
+
+      
+//         reverse(ans);
+
+//         return ans;
+//     }
+// };
