9cd1ba39919bbd2d42a44ca69eab665b2a07b2b9
Diksha Nautiyal <140512287+DikshaNautiyal06@users.noreply.github.com>
2025-03-19 12:08:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (4.89%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,20 +1,33 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        vector<int> arr; // Temporary array to store elements that are not equal to val
+// class Solution {
+// public:
+//     int removeElement(vector<int>& nums, int val) {
+//         vector<int> arr; 
         
-        // Collect elements that are not equal to val
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != val) {
-                arr.push_back(nums[i]);
+       
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] != val) {
+//                 arr.push_back(nums[i]);
+//             }
+//         }
+
+//         for (int i = 0; i < arr.size(); i++) {
+//             nums[i] = arr[i]; 
+//         }
+
+//         return arr.size(); 
+//     }
+// };
+
+class Solution{
+    public:
+    int removeElement(vector<int>& nums, int val){
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
             }
         }
-
-        // Copy elements from arr back to nums
-        for (int i = 0; i < arr.size(); i++) {
-            nums[i] = arr[i]; // Overwriting nums with the filtered values
-        }
-
-        return arr.size(); // Returning new length of nums
+        return k;
     }
 };
